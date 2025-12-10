567cd70038683823222ac60921a98059899593f2
jhqxxx <jhqxxx@163.com>
2025-12-10 13:06:53 +0800

    update some note

--- tests/test_qwen3vl.rs
+++ tests/test_qwen3vl.rs
@@ -50,7 +50,7 @@ fn qwen3vl_generate() -> Result<()> {
 
 #[tokio::test]
 async fn qwen3vl_stream() -> Result<()> {
-    // test with cuda: RUST_BACKTRACE=1 cargo test -F cuda qwen3vl_stream -r -- --nocapture
+    // test with cuda: RUST_BACKTRACE=1 cargo test -F cuda,ffmpeg qwen3vl_stream -r -- --nocapture
 
     let model_path = "/home/jhq/huggingface_model/Qwen/Qwen3-VL-2B-Instruct/";
 
